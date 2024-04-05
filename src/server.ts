57d17268ac0fcb188b3abcf9876a74639942bd4b
devfelipenunes <devfelipenunes@gmail.com>
2024-04-05 17:57:44 -0300

    update port

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ app.use(routers);
 
 AppDataSource.initialize().then(async () => {
   console.log("Data Source has been initialized!");
-  app.listen(PORT, () => {
+  app.listen(3000, () => {
     console.log("Server is running on port 3000");
   });
 });
